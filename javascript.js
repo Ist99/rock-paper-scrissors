924db28c7d30b0e02045f708da0b283ec8c64612
Ist99 <ibaranyai1999@gmail.com>
2022-11-29 00:39:04 +0100

    Added GUI to the game.

--- javascript.js
+++ javascript.js
@@ -4,83 +4,100 @@ function getComputerChoice(choices) {
 
 let choices = ['rock', 'paper', 'scrissors'];
 
+let a = 0;
+let b = 0;
 
 
-function playRound(computerSelection) {
-    let playerSelection = prompt('Rock? Paper? Scrissors? Choose your weapon!');
+
+function playRound(computerSelection, playerSelection) {
 
     if (playerSelection.toLowerCase() == 'rock') {
         if (computerSelection == 'rock') {
-            alert('Draw');
-            return(a,b);
+            div2.textContent = 'Draw';
         }
         else if (computerSelection == 'paper') {
-            alert('Loser! Paper beats rock! Muhahahahhhahahaha!');
-            return(a,++b);
+            div2.textContent = 'Loser! Paper beats rock! Muhahahahhhahahaha!';
+            ++b;
         }     
         else {
-            alert('You won! Rock beats scrissors!');
-            return(++a,b);
+            div2.textContent = 'You won! Rock beats scrissors!';
+            ++a;
         }
     }
 
     else if (playerSelection.toLowerCase() == 'paper') {
         if (computerSelection == 'paper') {
-            alert('Draw');
-            return(a,b);
+            div2.textContent = 'Draw';
         }
         else if (computerSelection == 'scrissors') {
-            alert('Loser! Scrissors beats paper! Muhahahahhhahahaha!');
-            return(a,++b);
+            div2.textContent = 'Loser! Scrissors beats paper! Muhahahahhhahahaha!';
+            ++b;
         }     
         else {
-            alert('You won! Paper beats rock!');
-            return(++a,b);
+            div2.textContent = 'You won! Paper beats rock!';
+            ++a;
         }
 }
 
     else if (playerSelection.toLowerCase() == 'scrissors') {
         if (computerSelection == 'scrissors') {
-            alert('Draw');
-            return(a,b);
+            div2.textContent = 'Draw';
     }
         else if (computerSelection == 'rock') {
-            alert('Loser! Rock beats scrissors! Muhahahahhhahahaha!');
-            return(a,++b);
+            div2.textContent = 'Loser! Rock beats scrissors! Muhahahahhhahahaha!';
+            ++b;
         }     
         else {
-            alert('You won! Scrissors beats paper!');
-            return(++a,b);
-    }
-} 
-
-    else {alert('FAIL! MINUS ONE POINT FOR YOU!');
-         return(--a,b);            
-}
-}    
-
-
-
-let a = 0;
-let b = 0;
-
-function play() {
-    for (let i = 0; i < 5; i++) {
-    (playRound(getComputerChoice(choices)));
-    }
-}
-
-alert("5 ROUNDS OF ROCK-PAPER-SCRISSORS! LET'S START!")
-play();
-if (a>b) {
-    alert('You WON! ' + a + ':' + b);
-}
-    else if (a==b) {
-        alert('DRAW! ' + a + ':' + b);
-    }
-        else {
-            alert('You LOST! ' + a + ':' + b);
+            div2.textContent = 'You won! Scrissors beats paper!';
+            ++a;
         }
+}
+
+div1.textContent = a + ' : ' + b;
+
+setTimeout (function () {
+    if (a == 5) {
+        div2.textContent = "YOU WON! " + a + ' : ' + b + "! PLAY AGAIN?";
+        a = 0; 
+        b = 0;
+        div1.textContent = a + ' : ' + b;
+    } else if (b == 5) {
+        div2.textContent = "YOU LOST! " + a + ' : ' + b + "! PLAY AGAIN?";
+        a = 0;
+        b = 0;
+        div1.textContent = a + ' : ' + b;
+        };
+    }, 1000) 
+}
 
 
+const container = document.querySelector('#container');
 
+for (let i = 1; i<=3; i++) {
+    container.appendChild(document.createElement('button')); 
+}
+
+const btn1 = document.querySelector('#container button:nth-of-type(1)');
+const btn2 = document.querySelector('#container button:nth-of-type(2)');
+const btn3 = document.querySelector('#container button:nth-of-type(3)');
+
+
+div1 = document.createElement('div');
+document.body.appendChild(div1);
+div1.textContent = a + ' : ' + b;
+
+div2 = document.createElement('div');
+document.body.appendChild(div2);
+div2.textContent = "START!";
+
+btn1.addEventListener("click", function() {
+    playRound(getComputerChoice(choices), 'rock');
+});
+
+btn2.addEventListener("click", function() {
+    playRound(getComputerChoice(choices), 'paper');
+});
+
+btn3.addEventListener("click", function() {
+    playRound(getComputerChoice(choices), 'scrissors');
+});
